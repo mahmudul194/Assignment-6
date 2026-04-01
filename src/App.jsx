98a3255a61bb360e6fda78b704c06498e17a4572
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-04-01 12:55:50 +0600

    added navbar in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+import Navbar from './components/Navbar'
+
+
 import './App.css'
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [view, setView] = useState('products');
+
 
   return (
     <>
-      
+      <div className="min-h-screen bg-white font-sans text-gray-900">
+        <Navbar cartCount={cart.length} setView={setView} />
+      </div>
     </>
   )
 }
